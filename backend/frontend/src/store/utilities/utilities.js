9b66ae1275aeaf9e7439bded778de4357642e9ca
Nicholsd Engleman <nicholas@englemaninvest.com>
2019-10-05 19:20:47 -0400

    split reducers up into seperate reducers

---
+++ backend/frontend/src/store/utilities/utilities.js
@@ -0,0 +1,14 @@
+export const updatedObject = (oldObject, updatedProperties) => {
+    return {
+        ...oldObject,
+        ...updatedProperties
+    };
+};
+
+export const findCurrentValueOfCrypto = (data, crypto_id) => {
+    const row = data.find(crypto => {
+        return crypto.crypto_id === crypto_id;
+    });
+    return row.data_value;
+};
+

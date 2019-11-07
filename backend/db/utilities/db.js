b19ca3bc6558f119a6d30811d70226028b0d52c3
Nicholsd Engleman <nicholas@englemaninvest.com>
2019-11-06 20:46:36 -0500

    reverted db credentials

--- backend/db/utilities/db.js
+++ backend/db/utilities/db.js
@@ -1,22 +1,22 @@
 const mysql = require("mysql");
 
-let db = mysql.createConnection({
-    host:
-        "ls-d2aa83b5cdb1edadcdb2601062dfa1b7182371f1.cb0z3ltmotsb.us-east-2.rds.amazonaws.com",
-    user: "cryptotigeradmin",
-    password: "-~s1UK~X:=yAr-b!_.[6ijo$YTvy>|}H",
-    database: "cryptotiger",
-    multipleStatements: true,
-    dateStrings: true
-});
-
 // let db = mysql.createConnection({
-//     host: "localhost",
-//     user: "cryptotiger",
-//     password: "G6wbEjtTCsA1IH9",
+//     host:
+//         "ls-d2aa83b5cdb1edadcdb2601062dfa1b7182371f1.cb0z3ltmotsb.us-east-2.rds.amazonaws.com",
+//     user: "cryptotigeradmin",
+//     password: "-~s1UK~X:=yAr-b!_.[6ijo$YTvy>|}H",
 //     database: "cryptotiger",
 //     multipleStatements: true,
 //     dateStrings: true
 // });
+
+let db = mysql.createConnection({
+    host: "localhost",
+    user: "cryptotiger",
+    password: "G6wbEjtTCsA1IH9",
+    database: "cryptotiger",
+    multipleStatements: true,
+    dateStrings: true
+});
 
 module.exports = db;
